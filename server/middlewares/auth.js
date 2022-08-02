8267124dbe7b1a9bf8a300fa8d4ba82d6196386b
Matthew D'Agostino <mattdags982@gmail.com>
2022-08-02 10:42:15 +0200

    changes

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,12 +1,9 @@
 const User = require("./../model/user.model");
 
 const authMiddleware = async (req, res, next) => {
-  // console.log(req.session.uid);
   try {
     const { uid } = req.session;
-    // console.log(uid);
     const user = await User.findOne({ _id: uid });
-    // console.log("user", user);
     if (!user) throw new Error();
     req.user = user;
     next();
